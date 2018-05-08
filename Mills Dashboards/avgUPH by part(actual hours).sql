b64a9ccbae5117405b66391a4085bb6b63d1f85c
Sean Burns <sean.burns@assaabloy.com>
2018-05-07 19:54:54 -0500

    added summation

--- Mills Dashboards/avgUPH by part(actual hours).sql
+++ Mills Dashboards/avgUPH by part(actual hours).sql
@@ -1,11 +1,7 @@
 
 
+
 select 
-[Part Number],
-sum([Ship Quantity]) as qty,
-SUM([Actual Hours]) as hrs,
-sum([Ship Quantity])/sum([Actual Hours]) as avgUPH
-from vWorkOrder
-where [Actual Hours] > 0
-group by [Part Number]
-order by 4 desc
+a.[Ops Date],a.[Work Center], a.[Part Number],a.[Ship Quantity],a.[Ship Quantity]/a.[Actual Hours] as UPH,b.avgUPH
+from vWorkOrder a left join vAverage_UPH b on b.[Part Number]=a.[Part Number]
+where a.[Actual Hours] >0
